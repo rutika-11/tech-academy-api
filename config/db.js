ebbfb6d1f537c7f991112170e6045ee81540575f
Rutika Tupe <rutika24cs@student.mes.ac.in>
2026-04-28 11:50:30 +0530

    update MongoDB config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected ✅");
     } catch (error) {
         console.error("DB Error ❌", error);
